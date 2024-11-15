bb3c36639a54d8ecae045a3f2e121a735b9eb15c
Amin <49003095+Amin0x@users.noreply.github.com>
2024-11-16 00:21:44 +0300

    Create Product.java

---
+++ src/main/java/com/alamin/ecommerce/product/Product.java
@@ -0,0 +1,94 @@
+package com.example.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Auto-generate the id value
+    private Long id;
+
+    @NotNull(message = "Product name is required")
+    @Size(min = 3, max = 100, message = "Product name must be between 3 and 100 characters")
+    private String name;
+
+    @Size(max = 255, message = "Description must be less than 255 characters")
+    private String description;
+
+    @NotNull(message = "Price is required")
+    private Double price;
+
+    @NotNull(message = "Stock is required")
+    private Integer stock;
+
+    // Default constructor (required by JPA)
+    public Product() {}
+
+    // Constructor for convenience
+    public Product(String name, String description, Double price, Integer stock) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", stock=" + stock +
+                '}';
+    }
+}
