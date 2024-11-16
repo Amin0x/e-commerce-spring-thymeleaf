ca255023bd280ad0c96f2f62cf5e121f32dd9633
Amin <49003095+Amin0x@users.noreply.github.com>
2024-11-16 11:22:50 +0300

    Create Invoice.java

---
+++ src/main/java/com/alamin/ecommerce/invoice/Invoice.java
@@ -0,0 +1,18 @@
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class Invoice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String customerName;
+    private Date invoiceDate;
+    private double totalAmount;
+
+    // Getters and Setters
+}
