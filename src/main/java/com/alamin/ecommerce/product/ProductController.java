73c73e89c59c3ea0c5e41da438d2dc695081cc01
Amin <49003095+Amin0x@users.noreply.github.com>
2024-11-17 00:20:50 +0300

    Update ProductController.java

--- src/main/java/com/alamin/ecommerce/product/ProductController.java
+++ src/main/java/com/alamin/ecommerce/product/ProductController.java
@@ -17,26 +17,56 @@ public class ProductController {
     private ProductRepository productRepository;
 
     // Create a new product
-    @PostMapping
+    @PostMapping("/api/products")
     public Product createProduct(@RequestBody Product product) {
         return productRepository.save(product);
     }
 
+    @GetMapping("/web/products/new")
+    public String showCreateProductForm(Model model) {
+        model.addAttribute("product", new Product());
+        return "create_product";
+    }
+
+    @PostMapping("/web/products")
+    public String createProduct(@Valid Product product, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "create_product";
+        }
+
+        productRepository.save(product);
+        return "redirect:/web/products";
+    }
+
     // Get all products
-    @GetMapping
+    @GetMapping("/api/products")
     public List<Product> getAllProducts() {
         return productRepository.findAll();
     }
 
+    // Get all products
+    @GetMapping("/web/products")
+    public String getAllProducts(@RequestParam(required = false) String category, Model model) {
+        //filter by category 
+        List<Product> products = productRepository.findAll();
+        if (category == null || category.isEmpty()){
+            products = null;
+        } else {
+            products = productRepository.findByCategory(category);
+        }
+        model.addAttribute("products", products);
+        return "products";
+    }
+
     // Get a product by ID
-    @GetMapping("/{id}")
+    @GetMapping("/api/products/{id}")
     public ResponseEntity<Product> getProductById(@PathVariable Long id) {
         Optional<Product> product = productRepository.findById(id);
         return product.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     // Update an existing product
-    @PutMapping("/{id}")
+    @PutMapping("/api/products/{id}")
     public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody Product product) {
         if (!productRepository.existsById(id)) {
             return ResponseEntity.notFound().build();
@@ -47,7 +77,7 @@ public class ProductController {
     }
 
     // Delete a product by ID
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/api/products/{id}")
     public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
         if (!productRepository.existsById(id)) {
             return ResponseEntity.notFound().build();
