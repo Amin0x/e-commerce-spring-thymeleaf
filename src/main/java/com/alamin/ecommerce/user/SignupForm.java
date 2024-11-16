3ea6df43c4ff731814be56381d577012c3611801
Amin <49003095+Amin0x@users.noreply.github.com>
2024-11-16 22:17:29 +0300

    Create SignupForm.java

---
+++ src/main/java/com/alamin/ecommerce/user/SignupForm.java
@@ -0,0 +1,32 @@
+package com.example.demo.model;
+
+public class SignupForm {
+    private String email;
+    private String password;
+    private String confirmPassword;
+
+    // Getters and Setters
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+}
