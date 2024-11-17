50bdc468f8d7929c38ac4efabcb3db64c07f0317
Amin <49003095+Amin0x@users.noreply.github.com>
2024-11-17 14:29:47 +0300

    Update ProductRepository.java

--- src/main/java/com/alamin/ecommerce/product/ProductRepository.java
+++ src/main/java/com/alamin/ecommerce/product/ProductRepository.java
@@ -9,4 +9,7 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     // Custom query methods (if needed) can be added here
     @Query(value = "SELECT * FROM Product p WHERE p.category = :category ORDER BY RAND() LIMIT 12", nativeQuery = true)
     List<Product> findRandomProductsByCategory(@Param("category") String category);
+
+    @Query(value = "SELECT * FROM Product p ORDER BY p.created_date DESC LIMIT 12", nativeQuery = true)
+    List<Product> findNewArrivalProducts();
 }
