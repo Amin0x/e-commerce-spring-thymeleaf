205081d6e20f7fdf79feb75d9dc9404fc8ccc97b
Amin <49003095+Amin0x@users.noreply.github.com>
2024-11-17 14:25:49 +0300

    Create ProductService.java

---
+++ src/main/java/com/alamin/ecommerce/product/ProductService.java
@@ -0,0 +1,19 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Product;
+import com.example.demo.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> getRandomProductsByCategory(String category) {
+        return productRepository.findRandomProductsByCategory(category);
+    }
+}
