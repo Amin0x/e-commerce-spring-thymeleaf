17e313ff165e16d6692dd4b0f83b7f69e821a56f
Amin <49003095+Amin0x@users.noreply.github.com>
2024-11-17 15:11:24 +0300

    Update HomeController.java

--- src/main/java/com/alamin/ecommerce/home/HomeController.java
+++ src/main/java/com/alamin/ecommerce/home/HomeController.java
@@ -6,8 +6,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+    @Autowired
+    private CategoryService categoryService;
+
     @GetMapping("/") // Handles requests to the root URL ("/")
-    public String home() {
+    public String home(Model model) {
+        List<Category> categories = categoryService.getRandomCategories();
+        Map<String, List<Category>> mapData = new HashMap<>();
+        for (Category category:categories){
+            List<Product> products = productService.getRandomProductsByCategory(category.getName());
+            mapData.add(category.getName(), products);
+        }
+
+        model.addAttribute("data", mapData);
         // Return the name of the HTML view (without .html extension)
         return "home"; // This will resolve to src/main/resources/templates/home.html
     }
