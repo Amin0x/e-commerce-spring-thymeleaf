de601ba0d8ad93c644a2a186884efa43cc82ff63
Amin <49003095+Amin0x@users.noreply.github.com>
2024-11-17 14:49:34 +0300

    Update CategoryRepository.java

--- src/main/java/com/alamin/ecommerce/category/CategoryRepository.java
+++ src/main/java/com/alamin/ecommerce/category/CategoryRepository.java
@@ -7,4 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     // You can add custom queries here if needed
+    @Query(value = "SELECT * FROM category ORDER BY RAND() LIMIT 12", nativeQuery = true)
+    List<Category> findRandomCategories();
 }
