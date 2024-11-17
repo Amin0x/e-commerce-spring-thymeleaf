2d856c8d01080b14e1a3cd0034b6d8ab41a4f76f
Amin <49003095+Amin0x@users.noreply.github.com>
2024-11-18 00:26:27 +0300

    Update HomeController.java

--- src/main/java/com/alamin/ecommerce/home/HomeController.java
+++ src/main/java/com/alamin/ecommerce/home/HomeController.java
@@ -15,7 +15,7 @@ public class HomeController {
     @GetMapping("/") // Handles requests to the root URL ("/")
     public String home(Model model) {
         List<Category> categories = categoryService.getRandomCategories();
-        Map<String, List<Category>> mapData = new HashMap<>();
+        /*Map<String, List<Category>> mapData = new HashMap<>();
         for (Category category:categories){
             List<Product> products = productService.getRandomProductsByCategory(category.getName());
 
@@ -23,7 +23,31 @@ public class HomeController {
                 mapData.add(category.getName(), products);
         }
 
-        model.addAttribute("data", mapData);
+        model.addAttribute("data", mapData);*/
+        
+        List<Product> products1 = productService.getRandomProductsByCategory(categories.getAt(0).getName());
+        List<Product> products2 = productService.getRandomProductsByCategory(categories.getAt(1).getName());
+        List<Product> products3 = productService.getRandomProductsByCategory(categories.getAt(2).getName());
+        List<Product> products4 = productService.getRandomProductsByCategory(categories.getAt(3).getName());
+
+        if(products1 != null){
+            model.addAttribute("products1", products1);
+            model.addAttribute("productsName1", categories.getAt(0).getName());
+        }
+        if(products2 != null){
+            model.addAttribute("products2", products2);
+            model.addAttribute("productsName2", categories.getAt(1).getName());
+        }
+        if(products3 != null){
+            model.addAttribute("products3", products3);
+            model.addAttribute("productsName3", categories.getAt(2).getName());
+        }
+        if(products4 != null){
+            model.addAttribute("products4", products4);
+            model.addAttribute("productsName4", categories.getAt(3).getName());
+        }
+        
+        
 
         List<Product> newProducts = productService.getNewArrivalProducts();
         model.addAttribute("newProducts", newProducts);
