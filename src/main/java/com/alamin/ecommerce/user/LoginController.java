06bd72849f6934acd550cbf0397287954eba7ebd
Amin <49003095+Amin0x@users.noreply.github.com>
2024-11-18 21:52:27 +0300

    Update LoginController.java

--- src/main/java/com/alamin/ecommerce/user/LoginController.java
+++ src/main/java/com/alamin/ecommerce/user/LoginController.java
@@ -10,14 +10,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class LoginController {
 
     @GetMapping("/login")
-    public String showLoginForm(Model model) {
+    public String showLoginForm(@RequestParam(require = false) String url, Model model) {
         model.addAttribute("loginForm", new LoginForm());
         return "login";
     }
 
     @PostMapping("/login")
-    public String processLogin(LoginForm loginForm) {
+    public String processLogin(@RequestParam(require = false) String url, LoginForm loginForm) {
         // Add your form processing logic here (e.g., authentication)
-        return "login_success";
+        return "redirect:home";
     }
 }
