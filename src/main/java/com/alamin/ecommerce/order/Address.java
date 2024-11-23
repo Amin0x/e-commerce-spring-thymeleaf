51a85f5de690e1950f9d8cc58d6bda9de2823e88
Amin <49003095+Amin0x@users.noreply.github.com>
2024-11-23 20:58:18 +0300

    Create Address.java

---
+++ src/main/java/com/alamin/ecommerce/order/Address.java
@@ -0,0 +1,84 @@
+package com.example.ecommerce.model;
+
+import javax.persistence.Embeddable;
+import javax.validation.constraints.NotEmpty;
+
+@Embeddable
+public class Address {
+
+    @NotEmpty
+    private String street;
+
+    @NotEmpty
+    private String city;
+
+    @NotEmpty
+    private String state;
+
+    @NotEmpty
+    private String country;
+
+    @NotEmpty
+    private String postalCode;
+
+    // Default constructor
+    public Address() {
+    }
+
+    // Constructor with parameters
+    public Address(String street, String city, String state, String country, String postalCode) {
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.country = country;
+        this.postalCode = postalCode;
+    }
+
+    // Getters and Setters
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    // Override toString() for better readability
+    @Override
+    public String toString() {
+        return street + ", " + city + ", " + state + ", " + country + " " + postalCode;
+    }
+}
