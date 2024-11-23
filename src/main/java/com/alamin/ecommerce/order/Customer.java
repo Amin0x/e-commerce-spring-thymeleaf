55a28f0cb7f07656c5ccba99015a726ac9e1c137
Amin <49003095+Amin0x@users.noreply.github.com>
2024-11-23 21:04:21 +0300

    Create Customer.java

---
+++ src/main/java/com/alamin/ecommerce/order/Customer.java
@@ -0,0 +1,78 @@
+package com.example.ecommerce.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty
+    private String firstName;
+
+    @NotEmpty
+    private String lastName;
+
+    @Embedded
+    private Address address;  // Embedding the Address class
+
+    // One-to-one relationship with User
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", unique = true)
+    private User user;
+
+    // Constructors
+    public Customer() {
+    }
+
+    public Customer(String firstName, String lastName, Address address, User user) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.user = user;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
