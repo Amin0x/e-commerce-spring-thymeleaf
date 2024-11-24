7097c0ebccde581935913f6c142799adf28e40fe
Amin <49003095+Amin0x@users.noreply.github.com>
2024-11-24 11:12:54 +0300

    Update Category.java

--- src/main/java/com/alamin/ecommerce/category/Category.java
+++ src/main/java/com/alamin/ecommerce/category/Category.java
@@ -1,6 +1,6 @@
-package com.example.demo.model;
+package com.alamin.ecommerce.category;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.List;
