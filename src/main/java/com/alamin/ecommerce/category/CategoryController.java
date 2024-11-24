af3b5c9457be37d6ee5973cee2dffdb0af94624c
Amin <49003095+Amin0x@users.noreply.github.com>
2024-11-24 11:30:49 +0300

    Update CategoryController.java

--- src/main/java/com/alamin/ecommerce/category/CategoryController.java
+++ src/main/java/com/alamin/ecommerce/category/CategoryController.java
@@ -1,7 +1,5 @@
 package com.alamin.ecommerce.category;
 
-import com.example.demo.model.Category;
-import com.example.demo.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
