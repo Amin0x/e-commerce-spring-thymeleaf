e229aad7c89df9e25da2d448cf4e654223d1f1aa
Amin <49003095+Amin0x@users.noreply.github.com>
2024-11-24 11:39:07 +0300

    Update OrderService.java

--- src/main/java/com/alamin/ecommerce/order/OrderService.java
+++ src/main/java/com/alamin/ecommerce/order/OrderService.java
@@ -1,7 +1,5 @@
 package com.alamin.ecommerce.order;
 
-import com.example.demo.model.Order;
-import com.example.demo.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
