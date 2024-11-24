c39acc99775be9ce0c21d35d0b20e9abf4fa8682
Amin <49003095+Amin0x@users.noreply.github.com>
2024-11-24 11:38:28 +0300

    Update OrderController.java

--- src/main/java/com/alamin/ecommerce/order/OrderController.java
+++ src/main/java/com/alamin/ecommerce/order/OrderController.java
@@ -1,7 +1,5 @@
 package com.alamin.ecommerce.order;
 
-import com.example.demo.model.Order;
-import com.example.demo.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
