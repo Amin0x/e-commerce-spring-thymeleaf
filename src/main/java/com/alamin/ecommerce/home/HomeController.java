cad16fbe43d00309c00c65896c81e20af9a39251
Amin <49003095+Amin0x@users.noreply.github.com>
2024-11-25 01:00:09 +0300

    Update HomeController.java

--- src/main/java/com/alamin/ecommerce/home/HomeController.java
+++ src/main/java/com/alamin/ecommerce/home/HomeController.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import java.util.Map;
 import java.util.List;
+import com.alamin.ecommerce.category.CategoryService;
+import com.alamin.ecommerce.product.ProductService;
+
 
 @Controller
 public class HomeController {
