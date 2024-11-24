c425769ab5ce108de6f83f32b5d762af4e9e8574
Amin <49003095+Amin0x@users.noreply.github.com>
2024-11-25 00:50:34 +0300

    Update HomeController.java

--- src/main/java/com/alamin/ecommerce/home/HomeController.java
+++ src/main/java/com/alamin/ecommerce/home/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import java.util.Map;
 import java.util.List;
 
