f840a973e23b4e369e8997e60eb7f85c00afde1c
Amin <49003095+Amin0x@users.noreply.github.com>
2024-11-25 22:22:22 +0300

    Update Config.java

--- src/main/java/com/alamin/ecommerce/config/Config.java
+++ src/main/java/com/alamin/ecommerce/config/Config.java
@@ -1,5 +1,12 @@
 package com.alamin.ecommerce.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import jakarta.sql.DataSource;
+import org.springframework.transaction.PlatformTransactionManager;
+
+
 @Configuration(proxyBeanMethods = false)
 @EnableJdbcHttpSession 
 public class Config {
