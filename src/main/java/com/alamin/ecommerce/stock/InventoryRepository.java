bc878255986a9e3d86e5b828a46413132faf166a
Amin <49003095+Amin0x@users.noreply.github.com>
2024-11-26 02:53:02 +0300

    Create InventoryRepository.java

---
+++ src/main/java/com/alamin/ecommerce/stock/InventoryRepository.java
@@ -0,0 +1,11 @@
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    // You can define custom query methods here if needed
+
+    // Example custom query method to find by name
+    Inventory findByName(String name);
+
+    // Example of querying for a list of inventory items with quantity greater than a specified value
+    List<Inventory> findByQuantityGreaterThan(int quantity);
+}
