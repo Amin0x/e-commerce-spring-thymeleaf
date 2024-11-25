daa71422acf78e07f3da1d5cc5706d518787c32e
Amin <49003095+Amin0x@users.noreply.github.com>
2024-11-25 22:50:57 +0300

    Update CategoryRepository.java

--- src/main/java/com/alamin/ecommerce/category/CategoryRepository.java
+++ src/main/java/com/alamin/ecommerce/category/CategoryRepository.java
@@ -2,6 +2,7 @@ package com.alamin.ecommerce.category;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 @Repository
