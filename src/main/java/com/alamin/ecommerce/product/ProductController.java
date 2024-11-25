dc89fd3e127ebcbe32758601dc93efbbd696773a
Amin <49003095+Amin0x@users.noreply.github.com>
2024-11-26 02:42:32 +0300

    Update ProductController.java

--- src/main/java/com/alamin/ecommerce/product/ProductController.java
+++ src/main/java/com/alamin/ecommerce/product/ProductController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.BindingResult;
 import jakarta.validation.Valid;
 import java.util.List;
 import java.util.Optional;
