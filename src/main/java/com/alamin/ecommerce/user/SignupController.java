2c63c315f954f85d58c50ff85a092f5e5788b0c5
Amin <49003095+Amin0x@users.noreply.github.com>
2024-11-25 22:44:04 +0300

    Update SignupController.java

--- src/main/java/com/alamin/ecommerce/user/SignupController.java
+++ src/main/java/com/alamin/ecommerce/user/SignupController.java
@@ -1,10 +1,10 @@
 package com.alamin.ecommerce.user;
 
-import com.example.demo.model.SignupForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import jakarta.validation.Valid;
 
 @Controller
 public class SignupController {
