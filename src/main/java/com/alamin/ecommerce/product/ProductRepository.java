6ae324c381fdbc8f5fd4550df9c8d80b25de47af
Amin <49003095+Amin0x@users.noreply.github.com>
2024-11-25 22:48:38 +0300

    Update ProductRepository.java

--- src/main/java/com/alamin/ecommerce/product/ProductRepository.java
+++ src/main/java/com/alamin/ecommerce/product/ProductRepository.java
@@ -2,6 +2,7 @@ package com.alamin.ecommerce.product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
