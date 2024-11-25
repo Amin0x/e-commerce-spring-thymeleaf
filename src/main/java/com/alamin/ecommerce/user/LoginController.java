d3d9123f4751bd4eab42f2179ec464d1b2fbca12
Amin <49003095+Amin0x@users.noreply.github.com>
2024-11-25 22:46:24 +0300

    Update LoginController.java

--- src/main/java/com/alamin/ecommerce/user/LoginController.java
+++ src/main/java/com/alamin/ecommerce/user/LoginController.java
@@ -1,12 +1,12 @@
 package com.alamin.ecommerce.user;
 
-import com.example.demo.model.LoginForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.validation.Valid;
 
 @Controller
 public class LoginController {
