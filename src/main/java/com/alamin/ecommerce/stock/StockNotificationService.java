382289eb8124e0824d761cb0cd03113130d3cb9a
Amin <49003095+Amin0x@users.noreply.github.com>
2024-11-26 16:27:18 +0300

    Update StockNotificationService.java

--- src/main/java/com/alamin/ecommerce/stock/StockNotificationService.java
+++ src/main/java/com/alamin/ecommerce/stock/StockNotificationService.java
@@ -3,6 +3,7 @@ package com.alamin.ecommerce.stock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 @Service
 public class StockNotificationService {
