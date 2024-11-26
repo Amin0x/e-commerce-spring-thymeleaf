3bd5563425c915053f703b9daf928e7c8c310f6f
Amin <49003095+Amin0x@users.noreply.github.com>
2024-11-26 16:30:38 +0300

    Update Inventory.java

--- src/main/java/com/alamin/ecommerce/stock/Inventory.java
+++ src/main/java/com/alamin/ecommerce/stock/Inventory.java
@@ -1,9 +1,10 @@
 package com.alamin.ecommerce.stock;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
 
 @Entity
 public class Inventory {
