b0a43181cf49040a44f56dba3cc851afbc64f471
Amin <49003095+Amin0x@users.noreply.github.com>
2024-11-27 15:44:41 +0300

    Update ProductService.java

--- src/main/java/com/alamin/ecommerce/product/ProductService.java
+++ src/main/java/com/alamin/ecommerce/product/ProductService.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Page.PageRequest;
+import org.springframework.data.domain.PageRequest;
 import java.util.List;
 
 @Service
