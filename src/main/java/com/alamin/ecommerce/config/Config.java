a95dc2b8605d11ab8f940249847b675c94178a07
Amin <49003095+Amin0x@users.noreply.github.com>
2024-11-28 09:54:57 +0300

    Update Config.java

--- src/main/java/com/alamin/ecommerce/config/Config.java
+++ src/main/java/com/alamin/ecommerce/config/Config.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 //import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import javax.sql.DataSource;
 import org.springframework.transaction.PlatformTransactionManager;
