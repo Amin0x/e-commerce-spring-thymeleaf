c56158d2c760b181021dbb45f3713d2cb5cceebe
Amin <49003095+Amin0x@users.noreply.github.com>
2024-11-29 20:23:16 +0300

    Update Category.java

--- src/main/java/com/alamin/ecommerce/category/Category.java
+++ src/main/java/com/alamin/ecommerce/category/Category.java
@@ -37,45 +37,4 @@ public class Category {
         this.description = description;
     }
 
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public List<Product> getProducts() {
-        return products;
-    }
-
-    public void setProducts(List<Product> products) {
-        this.products = products;
-    }
-
-    @Override
-    public String toString() {
-        return "Category{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", description='" + description + '\'' +
-                '}';
-    }
 }
