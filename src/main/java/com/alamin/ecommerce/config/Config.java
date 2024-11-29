0676a9cfacb11a162b4ccbcb33caf097815749ea
Amin <49003095+Amin0x@users.noreply.github.com>
2024-11-29 18:23:41 +0300

    Update Config.java

--- src/main/java/com/alamin/ecommerce/config/Config.java
+++ src/main/java/com/alamin/ecommerce/config/Config.java
@@ -15,17 +15,4 @@ import org.springframework.transaction.PlatformTransactionManager;
 //@EnableJdbcHttpSession 
 public class Config {
 
-	@Bean
-	public EmbeddedDatabase dataSource() {
-		return new EmbeddedDatabaseBuilder() 
-			.setType(EmbeddedDatabaseType.H2)
-			.addScript("org/springframework/session/jdbc/schema-h2.sql")
-			.build();
-	}
-
-	@Bean
-	public PlatformTransactionManager transactionManager(DataSource dataSource) {
-		return new DataSourceTransactionManager(dataSource); 
-	}
-
 }
