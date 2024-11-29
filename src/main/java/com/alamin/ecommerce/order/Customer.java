109111f065db6018ab77167329a653686612a688
Amin <49003095+Amin0x@users.noreply.github.com>
2024-11-29 17:08:34 +0300

    Update Customer.java

--- src/main/java/com/alamin/ecommerce/order/Customer.java
+++ src/main/java/com/alamin/ecommerce/order/Customer.java
@@ -38,44 +38,4 @@ public class Customer {
         this.user = user;
     }
 
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public Address getAddress() {
-        return address;
-    }
-
-    public void setAddress(Address address) {
-        this.address = address;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
 }
