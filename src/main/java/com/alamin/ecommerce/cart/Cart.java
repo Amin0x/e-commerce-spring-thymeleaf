1031b23924ea642db37d657805d6c0af08ed9ffd
Amin <49003095+Amin0x@users.noreply.github.com>
2024-11-29 16:21:01 +0300

    Update Cart.java

--- src/main/java/com/alamin/ecommerce/cart/Cart.java
+++ src/main/java/com/alamin/ecommerce/cart/Cart.java
@@ -2,6 +2,7 @@ package com.alamin.ecommerce.cart;
 
 import jakarta.persistence.*;
 import java.util.List;
+import lombok.*;
 
 @Entity
 public class Cart {
@@ -16,36 +17,6 @@ public class Cart {
     private int totalQuantity;
     private double totalPrice;
 
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public List<CartItem> getItems() {
-        return items;
-    }
-
-    public void setItems(List<CartItem> items) {
-        this.items = items;
-    }
-
-    public int getTotalQuantity() {
-        return totalQuantity;
-    }
-
-    public void setTotalQuantity(int totalQuantity) {
-        this.totalQuantity = totalQuantity;
-    }
-
-    public double getTotalPrice() {
-        return totalPrice;
-    }
-
-    public void setTotalPrice(double totalPrice) {
-        this.totalPrice = totalPrice;
-    }
+    public Cart(){}
+    
 }
