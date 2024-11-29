a93b37438abec1a51d02ec123ab64c91edc4704d
Amin <49003095+Amin0x@users.noreply.github.com>
2024-11-29 16:16:57 +0300

    Update Product.java

--- src/main/java/com/alamin/ecommerce/product/Product.java
+++ src/main/java/com/alamin/ecommerce/product/Product.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.*;
 
 @Entity
 @Table(name = "products")
