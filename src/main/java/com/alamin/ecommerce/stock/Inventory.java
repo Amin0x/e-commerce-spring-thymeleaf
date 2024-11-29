1d6e2c6c87c6d2861c16fec3e6cef3724bbe03f7
Amin <49003095+Amin0x@users.noreply.github.com>
2024-11-29 16:35:51 +0300

    Update Inventory.java

--- src/main/java/com/alamin/ecommerce/stock/Inventory.java
+++ src/main/java/com/alamin/ecommerce/stock/Inventory.java
@@ -4,8 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import lombok.*;
 
-
+@Data
 @Entity
 public class Inventory {
 
@@ -16,37 +17,6 @@ public class Inventory {
     private String name;
     private int quantity;
     private double price;
+    private Long productId;
 
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
 }
