74db9f8dfca2cf1817bd16dee683986483d3bc93
Amin <49003095+Amin0x@users.noreply.github.com>
2024-11-29 16:57:41 +0300

    Update OrderItem.java

--- src/main/java/com/alamin/ecommerce/order/OrderItem.java
+++ src/main/java/com/alamin/ecommerce/order/OrderItem.java
@@ -3,7 +3,9 @@ package com.alamin.ecommerce.order;
 import com.alamin.ecommerce.product.Product;
 import jakarta.persistence.*;
 import java.math.BigDecimal;
+import lombok.*;
 
+@Data
 @Entity
 public class OrderItem {
 
@@ -28,48 +30,6 @@ public class OrderItem {
         this.order = order;
         this.product = product;
         this.quantity = quantity;
-        this.price = price;
-    }
-
-    // Getters and Setters
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Order getOrder() {
-        return order;
-    }
-
-    public void setOrder(Order order) {
-        this.order = order;
-    }
-
-    public Product getProduct() {
-        return product;
-    }
-
-    public void setProduct(Product product) {
-        this.product = product;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
         this.price = price;
     }
 }
