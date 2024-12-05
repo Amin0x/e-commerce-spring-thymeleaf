7f2ab6489e97068cbce2220e14ef763ab9bcf825
Amin <49003095+Amin0x@users.noreply.github.com>
2024-12-05 22:24:08 +0300

    Update OrderService.java

--- src/main/java/com/alamin/ecommerce/order/OrderService.java
+++ src/main/java/com/alamin/ecommerce/order/OrderService.java
@@ -2,7 +2,9 @@ package com.alamin.ecommerce.order;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Page;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,9 +14,19 @@ public class OrderService {
     @Autowired
     private OrderRepository orderRepository;
 
-    public List<Order> getAllOrders() {
-        return orderRepository.findAll();
-    }
+    
+
+    public List<Order> getAllOrders(int page, int size, int order) {
+   		// Create a PageRequest with pagination and sorting
+   		Sort sort = Sort.by(order == 1 ? Sort.Order.asc("date") : Sort.Order.desc("date"));
+   		PageRequest pageRequest = PageRequest.of(page, size, sort);
+
+   		// Fetch the paged and sorted orders from the repository
+   		Page<Order> orderPage = orderRepository.findAll(pageRequest);
+    
+   		// Return the list of orders (you can return the Page if you need pagination info)
+   		return orderPage.getContent();
+	}
 
     public Optional<Order> getOrderById(Long id) {
         return orderRepository.findById(id);
