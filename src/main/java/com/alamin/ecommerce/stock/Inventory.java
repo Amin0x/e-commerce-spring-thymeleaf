46cd206fdff7b15b94a3bcbcb7e0abd9d33ad2b2
Amin <49003095+Amin0x@users.noreply.github.com>
2024-12-13 00:05:13 +0300

    Update Inventory.java

--- src/main/java/com/alamin/ecommerce/stock/Inventory.java
+++ src/main/java/com/alamin/ecommerce/stock/Inventory.java
@@ -14,7 +14,7 @@ public class Inventory {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private int quantity;
-    private Integer price;
+    private int price;
     private Long productId;
     private LocalDateTime created;
     private LocalDateTime updated;
