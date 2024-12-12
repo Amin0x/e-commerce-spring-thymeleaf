fde88577c69e29d403bfa9e49f6f0802d82b8399
Amin <49003095+Amin0x@users.noreply.github.com>
2024-12-12 23:56:38 +0300

    Update InventoryRepository.java

--- src/main/java/com/alamin/ecommerce/stock/InventoryRepository.java
+++ src/main/java/com/alamin/ecommerce/stock/InventoryRepository.java
@@ -7,7 +7,7 @@ public interface InventoryRepository extends JpaRepository<Inventory, Long> {
     // You can define custom query methods here if needed
 
     // Example custom query method to find by name
-    Inventory findByName(String name);
+    Inventory findByProductId(Long id);
 
     // Example of querying for a list of inventory items with quantity greater than a specified value
     List<Inventory> findByQuantityGreaterThan(int quantity);
