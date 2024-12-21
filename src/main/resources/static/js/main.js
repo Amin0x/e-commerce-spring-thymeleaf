4d3966c3e717631f48c4696088aa83169d896a40
Amin <49003095+Amin0x@users.noreply.github.com>
2024-12-22 02:11:14 +0300

    Update main.js

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,1 +1,11 @@
+const signupForm = () => {
 
+  document.getElementById('signupForm').addEventListener('submit', function(event) {
+            var password = document.getElementById('password').value;
+            var confirmPassword = document.getElementById('confirmPassword').value;
+
+            if (password !== confirmPassword) {
+                alert('Passwords do not match.');
+                event.preventDefault(); // Prevent form submission
+            }
+})};
