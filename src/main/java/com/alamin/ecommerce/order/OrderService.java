4910c34bc3812f5f9fd9225a1e979a7ee52d3eb4
Amin <49003095+Amin0x@users.noreply.github.com>
2024-12-22 12:23:49 +0300

    Update OrderService.java

--- src/main/java/com/alamin/ecommerce/order/OrderService.java
+++ src/main/java/com/alamin/ecommerce/order/OrderService.java
@@ -79,9 +79,16 @@ public class OrderService {
 	
 
 
+
 	@Scheduled(cron = "0 0 0 * * *")
     public void runAtMidnight() {
         System.out.println("Task is running at midnight!");
+    }
+	
+	// run at midnight on the last day of the month
+    @Scheduled(cron = "0 0 0 L * ?")
+    public void runOnLastDayOfMonth() {
+        System.out.println("Task is running on the last day of the month at midnight!");
     }
 
 	
